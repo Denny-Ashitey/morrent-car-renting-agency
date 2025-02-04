0d6e0fc88c677a9dc2dc4f80c84c668f60b1aec8
Denzel Ashitey <denzelniiarmahashiteycode@gmail.com>
2025-02-04 18:29:26 +0000

    navigation fix

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,10 +8,16 @@ export default function Nav() {
                 <Search />
             </div>
             <div className="flex items-center gap-8">
-                <img src="/src/assets/heart.png" alt='favourites' />
-                <img src='/src/assets/notification.png' alt='notifications' />
-                <img src="/src/assets/setting-2.png" alt="settings" />
-                <img src="/src/assets/profile.png" alt="profile" />
+                <div className="border-[1px] p-2 rounded-3xl">
+                    <img className="cursor-pointer" src='/src/assets/notification.png' alt='notifications' />
+                </div>
+                <div className="border-[1px] p-2 rounded-3xl">
+                    <img className="cursor-pointer" src="/src/assets/setting-2.png" alt="settings" />
+                </div>
+                <div className="border-[1px] p-2 rounded-3xl">
+                    <img className="cursor-pointer" src="/src/assets/heart.png" alt='favourites' />
+                </div>
+                    <img className="cursor-pointer" src="/src/assets/profile.png" alt="profile" />                
             </div>
         </nav>
     )
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,8 +2,8 @@ export default function Search() {
     return (
         <div className="flex items-center gap-4 border-solid border-[1px] border-[#c3c4e940] px-[20px] py-[12px] rounded-3xl">
             <img src="/src/assets/search-normal.png" />
-            <input className="w-96" placeholder="Search Something Here"/>
-            <img src="/src/assets/filter.png" />
+            <input className="w-96 outline-none" placeholder="Search Something Here"/>
+            <img className="cursor-pointer" src="/src/assets/filter.png" />
         </div>
     )
 }
